7579b99fada75334fcd43a5e24c1d55a6a2a167f
André Alves de Aguiar <51351450+AndreAlvesdeAguiar@users.noreply.github.com>
2024-05-15 01:11:19 -0300

    Update index.vue

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -8,7 +8,7 @@
 </template>
 <script setup lang="ts">
 const { data: posts, pending } = await useFetch(
-  "https://andre-alvesde-aguiar-github-io.vercel.app/pt-BR/blog/link-com-vercel-3f46",
+  "https://dev.to/dehkeep/link-com-vercel-3f46",
 );
 </script>
 <style scoped></style>
