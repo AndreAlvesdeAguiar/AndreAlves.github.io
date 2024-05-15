8966f0b36513cf852d6b0dacf3b8dcda7ffc5f9d
André Alves de Aguiar <51351450+AndreAlvesdeAguiar@users.noreply.github.com>
2024-05-15 13:09:58 -0300

    .

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,26 +1,14 @@
-<script>
-import feather from "feather-icons";
-
-export default {
-  name: "About",
-  scrollToTop: true,
-  mounted() {
-    feather.replace();
-  },
-  updated() {
-    feather.replace();
-  },
-};
-</script>
-
 <template>
-  <div>
-    <AboutMe class="container mx-auto" />
-
-    <AboutSkills />
-
-    <!-- <AboutClients class="container mx-auto" /> -->
+  <div
+    class="flex flex-col sm:justify-between items-center dark:bg-ternary-dark md:px-10 md:py-10"
+  >
+    <BlogGridSkeleton v-if="pending" />
+    <BlogGrid v-else :posts="posts" />
   </div>
 </template>
-
+<script setup lang="ts">
+const { data: posts, pending } = await useLazyFetch(
+  "https://dev.to/api/articles?username=lfxa&state=all",
+);
+</script>
 <style scoped></style>
