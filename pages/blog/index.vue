90ce544f4bd541e04d80f809eaf6668a389b5d32
André Alves de Aguiar <51351450+AndreAlvesdeAguiar@users.noreply.github.com>
2024-05-15 14:53:08 -0300

    Update index.vue

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,16 +4,17 @@
   >
     <h1>Meus Posts</h1>
 
-    <div v-if="pending">
+    <!-- <div v-if="!pending">
       <div v-for="post in posts" :key="post.id">
         <h2>{{ post.title }}</h2>
         <p>{{ post.description }}</p>
         <a :href="post.url" target="_blank">Ver post</a>
       </div>
-    </div>
+    </div> -->
 
-    <BlogGridSkeleton v-if="!pending" />
+    <BlogGridSkeleton v-if="pending" />
     <BlogGrid v-else :posts="posts" />
+    
   </div>
 </template>
 
