365ac1649efbeccd67dad73c27b74f9de29ab97f
André Alves de Aguiar <51351450+AndreAlvesdeAguiar@users.noreply.github.com>
2024-05-15 15:05:39 -0300

    Update index.vue

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -6,21 +6,9 @@
     <BlogGrid v-else :posts="posts" />
   </div>
 </template>
-
 <script setup lang="ts">
-// Importe a função useLazyFetch da sua biblioteca
-import { useLazyFetch } from 'lazy-fetch-lib';
-
-// Defina a função que busca os artigos
-const fetchArticles = async () => {
-  try {
-    const response = await fetch('https://dev.to/api/articles?username=lfxa&state=all');
-    return await response.json();
-  } catch (error) {
-    throw new Error('Erro ao carregar os artigos: ' + error.message);
-  }
-};
-
-// Use o hook useLazyFetch para buscar os artigos
-const { data: posts, pending } = useLazyFetch(fetchArticles);
+const { data: posts, pending } = await useLazyFetch(
+  "https://dev.to/api/articles?username=lfxa&state=all",
+);
 </script>
+<style scoped></style>
