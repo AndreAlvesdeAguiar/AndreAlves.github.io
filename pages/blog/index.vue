8fe4b536dce169384a753a8f833c5d1eece30374
André Alves de Aguiar <51351450+AndreAlvesdeAguiar@users.noreply.github.com>
2024-05-15 14:42:26 -0300

    Update index.vue

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,39 +1,35 @@
 <template>
-  <div
-    class="flex flex-col sm:justify-between items-center dark:bg-ternary-dark md:px-10 md:py-10">
-    <h1>Meus Posts</h1>   
+  <div class="flex flex-col sm:justify-between items-center dark:bg-ternary-dark md:px-10 md:py-10">
+    <h1>Meus Posts</h1>
+
+    <div v-if="!pending">
+      <div v-for="post in posts" :key="post.id">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.description }}</p>
+        <a :href="post.url" target="_blank">Ver post</a>
+      </div>
+    </div>
+
+    <BlogGridSkeleton v-if="pending" />
+    <BlogGrid v-else :posts="posts" />
   </div>
 </template>
-      <div v-if="post in posts" :key="post.id">
-        <h2>{{ post.title }}</h2>
-        <p>{{ post.description }}</p>
-        <a :href="post.url" target="_blank">Ver post</a>
-      </div>
-      <div BlogGridSkeleton v-if="pending">
-        <h2>{{ post.title }}</h2>
-        <p>{{ post.description }}</p>
-        <a :href="post.url" target="_blank">Ver post</a>
-      </div>
-      <div BlogGrid v-else :posts="posts">
-        <h2>{{ post.title }}</h2>
-        <p>{{ post.description }}</p>
-        <a :href="post.url" target="_blank">Ver post</a>
-      </div>
+
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 
 const posts = ref([]);
-const loading = ref(true);
+const pending = ref(true);
 
 onMounted(async () => {
   try {
     const response = await fetch('https://dev.to/api/articles?username=dehkeep');
     const data = await response.json();
     posts.value = data;
-    loading.value = false;
+    pending.value = false;
   } catch (error) {
     console.error('Erro ao carregar os posts:', error);
-    loading.value = false;
+    pending.value = false;
   }
 });
 </script>
