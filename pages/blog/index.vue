4e7fcb04a93cba5d186833adcab34d0c0b734cd1
André Alves de Aguiar <51351450+AndreAlvesdeAguiar@users.noreply.github.com>
2024-05-15 13:40:40 -0300

    Update index.vue

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,28 +1,30 @@
-
 <template>
-  <div
-    class="flex flex-col sm:justify-between items-center dark:bg-ternary-dark md:px-10 md:py-10"
-  >
+  <div class="flex flex-col sm:justify-between items-center dark:bg-ternary-dark md:px-10 md:py-10">
     <BlogGridSkeleton v-if="pending" />
     <BlogGrid v-else :posts="posts" />
   </div>
 </template>
+
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
+import { useLazyFetch } from 'your-lazy-fetch-lib'; // Importe sua biblioteca de busca preguiçosa
 
 const posts = ref([]);
-const loading = ref(true);
+const pending = ref(true);
 
 onMounted(async () => {
   try {
     const response = await fetch('https://dev.to/api/articles?username=dehkeep');
     const data = await response.json();
     posts.value = data;
-    loading.value = false;
+    pending.value = false;
   } catch (error) {
     console.error('Erro ao carregar os posts:', error);
-    loading.value = false;
+    pending.value = false;
   }
 });
 </script>
-<style scoped></style>
+
+<style scoped>
+/* Estilos específicos para este componente */
+</style>
