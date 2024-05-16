2cff42b48695fec6e046e227e0fec91ee89892ff
André Alves de Aguiar <51351450+AndreAlvesdeAguiar@users.noreply.github.com>
2024-05-16 00:50:19 -0300

    x

--- i18n/skills.js
+++
@@ -1,7 +0,0 @@
-export const skillsPTBR = {
-  certificateAluraDotNet: "Certificado de formação Web Development - Le Wagon",
-};
-
-export const skillsENUS = {
-  certificateAluraDotNet: "Web Development training certificate from - Le Wagon",
-};
